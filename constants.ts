44c54bd7536b280292fea45c2cf6a318154f41a2
Gianluigi Tiesi <sherpya@netfarm.it>
2023-03-01 19:37:41 +0100

    fixed config and typings

--- constants.ts
+++ constants.ts
@@ -3,8 +3,8 @@ import { ethers } from 'hardhat';
 export const SENDER_WINS = 1;
 export const RECEIVER_WINS = 2;
 
-export const FEE_TIMEOUT = 302400;
-export const FEE_RECIPIENT_BASISPOINT = 550;
+export const FEE_TIMEOUT = process.env.NERWO_FEE_TIMEOUT;
+export const FEE_RECIPIENT_BASISPOINT = process.env.NERWO_FEE_RECIPIENT_BASISPOINT;
 
 export const TIMEOUT_PAYMENT = 1500;
 
--- environment.d.ts
+++ environment.d.ts
@@ -1,10 +1,7 @@
 declare global {
     namespace NodeJS {
         interface ProcessEnv {
-            RPC_URL: string;
             PRIVATE_KEY: string;
-            HARDHAT_MNEMONIC: string;
-            ETHERSCAN_API_KEY: string;
             NERWO_COURT_ADDRESS: string;
             NERWO_PLATFORM_ADDRESS: string;
             NERWO_ARBITRATION_PRICE: string;
