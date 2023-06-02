fa86410ed19ca1d9cc148d87caf4312b87c505ec
Gianluigi Tiesi <sherpya@gmail.com>
2023-06-02 15:39:07 +0200

    tests: use random with fixed seed for gas calc

--- test/utils.ts
+++ test/utils.ts
@@ -79,9 +79,26 @@ export async function createDispute(sender: Signer, receiver: Signer, transactio
     return events.at(-1)!.args!._disputeID!;
 }
 
+function sfc32(a: number, b: number, c: number, d: number) {
+    return function () {
+        a >>>= 0; b >>>= 0; c >>>= 0; d >>>= 0;
+        let t = (a + b) | 0;
+        a = b ^ b >>> 9;
+        b = c + (c << 3) | 0;
+        c = (c << 21 | c >>> 11);
+        d = d + 1 | 0;
+        t = t + d | 0;
+        c = c + t | 0;
+        return (t >>> 0) / 4294967296;
+    };
+}
+
+// a real random function screws gas calculations
+const rand = sfc32(0x9e3779b9, 0x243f6a88, 0xb7e15162, 42 ^ 1337);
+
 export async function randomAmount() {
-    const minimalAmount = BigNumber.from(10);
-    return minimalAmount.mul(Math.floor(10 / Math.random()));
+    const minimalAmount = BigNumber.from(100000000000000);
+    return minimalAmount.mul(Math.floor(100000 / rand()));
 }
 
 // https://ethereum.stackexchange.com/a/123567/115740
