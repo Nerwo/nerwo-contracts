286f788e64a3cf7c94c82d8801c8484d1c2748e4
Gianluigi Tiesi <sherpya@gmail.com>
2023-08-16 10:16:48 +0200

    deployments

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -42,18 +42,18 @@ const config: HardhatUserConfig = {
     goerli: {
       url: 'https://ethereum-goerli.publicnode.com',
       accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : undefined,
-      deploy: ['deploy-tokens', 'deploy']
+      deploy: ['deploy']
     },
     sepolia: {
       url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_SEPOLIA_KEY || ''}`,
       accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : undefined,
-      deploy: ['deploy-tokens', 'deploy']
+      deploy: ['deploy']
     },
     hardhat: {
       accounts: {
         mnemonic: process.env.HARDHAT_MNEMONIC || HARDHAT_NETWORK_MNEMONIC
       },
-      deploy: ['deploy-tokens', 'deploy']
+      deploy: ['deploy-test', 'deploy']
     },
     buildbear: {
       url: `https://rpc.buildbear.io/${BUILDBEAR_CONTAINER_NAME}`,
