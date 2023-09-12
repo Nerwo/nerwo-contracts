f29daa90e5e9c108546346e97aae890c39f2a9eb
Gianluigi Tiesi <sherpya@gmail.com>
2023-09-12 11:52:40 +0200

    test token: use distinctive name an symbol, added burn()

--- contracts/NerwoTetherToken.sol
+++ contracts/NerwoTetherToken.sol
@@ -11,7 +11,7 @@ pragma solidity ^0.8.0;
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract NerwoTetherToken is ERC20 {
-    constructor() ERC20("Tether USD", "USDT") {}
+    constructor() ERC20("Nerwo Test USDT", "USDT.n") {}
 
     function decimals() public pure override returns (uint8) {
         return 6;
@@ -19,5 +19,9 @@ contract NerwoTetherToken is ERC20 {
 
     function mint(uint256 amount) external {
         _mint(msg.sender, amount);
+    }
+
+    function burn(uint256 amount) external {
+        _burn(msg.sender, amount);
     }
 }
